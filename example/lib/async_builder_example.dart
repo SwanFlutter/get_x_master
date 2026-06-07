4bed4c7b3596098687ff30a6e9f26afed9dd6453
SwanFlutter1993 <swan.dev1993@gmail.com>
2026-06-07 00:12:39 -0700

    edit

--- example/lib/async_builder_example.dart
+++ example/lib/async_builder_example.dart
@@ -253,7 +253,7 @@ class AsyncBuilderExample extends StatelessWidget {
       border: Border.all(color: Colors.grey[200]!),
       boxShadow: [
         BoxShadow(
-          color: Colors.black.withValues(alpha:  0.03),
+          color: Colors.black.withValues(alpha: 0.03),
           blurRadius: 10,
           offset: const Offset(0, 4),
         )
